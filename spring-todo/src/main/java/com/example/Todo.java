28ec956e7113e370d91dd16dee5a369166d28adc
Thomas Qvarnström <tqvarnst@redhat.com>
2019-11-14 15:42:57 +0100

    to be squashed

--- spring-todo/src/main/java/com/example/Todo.java
+++ spring-todo/src/main/java/com/example/Todo.java
@@ -1,61 +1,35 @@
 package com.example;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Todo {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter @Setter
     private Long id;
+
     @NotBlank
     @Column(unique = true)
+    @Getter @Setter
     private String title;
+
+    @Getter @Setter
     private boolean completed;
+
     @Column(name = "ordering")
+    @Getter @Setter
     private int order;
+
+    @Getter @Setter
     private String url;
 
     public Todo() {
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public boolean isCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(boolean completed) {
-        this.completed = completed;
-    }
-
-    public int getOrder() {
-        return order;
-    }
-
-    public void setOrder(int order) {
-        this.order = order;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
 }
